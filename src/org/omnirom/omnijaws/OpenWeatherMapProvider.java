06f887040a0dba21947e89fdac0360d6af458e51
mydongistiny <jaysonedson@gmail.com>
2016-03-30 16:47:26 -0700

    DON'T MERGE - Change OWM API key
    If you want to use OpenWeatherMap just go create a free account at openweathermap.org and get a key.

--- src/org/omnirom/omnijaws/OpenWeatherMapProvider.java
+++ src/org/omnirom/omnijaws/OpenWeatherMapProvider.java
@@ -38,7 +38,7 @@ public class OpenWeatherMapProvider extends AbstractWeatherProvider {
     private static final int FORECAST_DAYS = 5;
     private static final String SELECTION_LOCATION = "lat=%f&lon=%f";
     private static final String SELECTION_ID = "id=%s";
-    private static final String API_KEY = "9f65b19b7a6648346dda93c6973a682c";
+    private static final String API_KEY = "af6b64750bf392c94ec8ff16e6b2d41a";
 
     private static final String URL_LOCATION =
             "http://api.openweathermap.org/data/2.5/find?q=%s&mode=json&lang=%s&appid=%s";
