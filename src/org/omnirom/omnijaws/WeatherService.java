cdbf04e4cc1609b315617873b0d505e4555f2824
maxwen <max.weninger@gmail.com>
2017-11-03 00:15:41 +0100

    OmniJaws: service stop follow same path as if disabled from outside
    so that disable broadcast is sent
    
    Change-Id: Ief7755edf8dca60f7c2524602528cf8b841fd281
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- src/org/omnirom/omnijaws/WeatherService.java
+++ src/org/omnirom/omnijaws/WeatherService.java
@@ -127,7 +127,9 @@ public class WeatherService extends Service {
 
     public static void stop(Context context) {
         Intent i = new Intent(context, WeatherService.class);
-        context.stopService(i);
+        i.setAction(ACTION_ENABLE);
+        i.putExtra(EXTRA_ENABLE, false);
+        context.startService(i);
     }
 
     private static PendingIntent alarmPending(Context context) {
